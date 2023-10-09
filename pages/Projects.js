1cb45f6bcae72438450f61edef191611b19148d5
Kyung19 <kyung19@students.ubc.bc.ca>
2023-10-09 01:15:44 -0700

    adding project cards

--- pages/Projects.js
+++ pages/Projects.js
@@ -6,23 +6,22 @@ const Projects = () => {
     <div className="container bg-base">
     <div className="grid min-h-screen">
       <div className="py-20"> 
-        <div className="font-extrabold text-5xl text-black mb-10 ml-20">Hi, welcome to my website!</div>
+        <div className="font-extrabold text-5xl text-black mb-10 ml-10">Projects</div>
 
           <div className="grid grid-cols-2 grid-flow-row gap-5 ml-10">
 
           <div className="card w-96 bg-base-100 shadow-xl">
           <Image
-                        src="/ubc.png" 
-                        height={100}
-                        width={100}
-                        alt="ubc image"
+                        src="/Inteflow.png" 
+                        height={450}
+                        width={550}
+                        alt="inteflow image"
                     />
             <div className="card-body">
-                <h2 className="card-title">
-                Shoes!
-                <div className="badge badge-secondary">NEW</div>
+                <h2 className="card-title">Inteflow
+                    <div className="badge badge-secondary">Aug, 2023</div>
                 </h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <p></p>
             <div className="card-actions justify-end">
             <div className="badge badge-outline">Fashion</div> 
             <div className="badge badge-outline">Products</div>
@@ -30,23 +29,43 @@ const Projects = () => {
             </div>
           </div>
 
-            <div className="card w-150 bg-button text-white shadow-xl">
-              <div className="card-body">
-                <h2 className="card-title">Hi, welcome to my website!</h2>
-                <p>I&apos;m Kennedy :)</p>
-                <p>I&apos;m currently a student at the University of British Columbia</p>
-                <div className="card-actions justify-end"></div>
-              </div>
+          <div className="card w-96 bg-base-100 shadow-xl">
+          <Image
+                        src="/kizuna.jpg" 
+                        height={450}
+                        width={500}
+                        alt="ubc image"
+                    />
+            <div className="card-body">
+                <h2 className="card-title">Kizuna
+                <div className="badge badge-secondary">Jan, 2023</div>
+                </h2>
+            <p></p>
+            <div className="card-actions justify-end">
+            <div className="badge badge-outline">Fashion</div> 
+            <div className="badge badge-outline">Products</div>
             </div>
+            </div>
+          </div>
 
-            <div className="card w-150 bg-button text-white shadow-xl">
-              <div className="card-body">
-                <h2 className="card-title">Hi, welcome to my website!</h2>
-                <p>I&apos;m Kennedy :)</p>
-                <p>I&apos;m currently a student at the University of British Columbia</p>
-                <div className="card-actions justify-end"></div>
-              </div>
+          <div className="card w-96 bg-base-100 shadow-xl">
+          <Image
+                        src="/pomo.PNG" 
+                        height={450}
+                        width={500}
+                        alt="ubc image"
+                    />
+            <div className="card-body">
+                <h2 className="card-title">Pomo's Paradise
+                <div className="badge badge-secondary">Feb, 2023</div>
+                </h2>
+            <p></p>
+            <div className="card-actions justify-end">
+            <div className="badge badge-outline">Fashion</div> 
+            <div className="badge badge-outline">Products</div>
             </div>
+            </div>
+          </div>
 
           </div>
 
