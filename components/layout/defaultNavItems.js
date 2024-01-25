000bbfbedd1f6d0bf8caadab8ad02db68f9ad27c
Kennedy Yung <97806971+kennedyyung@users.noreply.github.com>
2024-01-25 00:09:36 -0800

    Update defaultNavItems.js

--- components/layout/defaultNavItems.js
+++ components/layout/defaultNavItems.js
@@ -23,7 +23,7 @@ export const defaultNavItems = [
   },
   {
     label: "Resume",
-    href: "https://drive.google.com/file/d/12a9glpo9-Xu8Nnj-Zl8fUGtEo0NBvdyC/view?usp=sharing",
+    href: "https://drive.google.com/file/d/1_brSqVhScD-YEhd3aSr6CskTAB1smtxh/view?usp=sharing",
     rel: "noopener noreferrer",
     target: "_blank",
     icon: <DocumentTextIcon className="w-6 h-6" />,
