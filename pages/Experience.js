8e6e10a56fbb076fe47f13c8e28d78dcb3a1ad42
Kennedy Yung <97806971+kennedyyung@users.noreply.github.com>
2024-02-10 22:38:08 -0800

    adding to the experience page

--- pages/Experience.js
+++ pages/Experience.js
@@ -4,15 +4,23 @@ const Experience = () => {
     return (
         <div className="container min-h-screen bg-base mb-10">
             <div className="grid min-h-screen">
-                <div className="py-19" id="experience">
-                    <div className="font-extrabold text-5xl text-black mb-10 ml-10">Experience</div>
-                    <hr/>
+                <div className="py-19 text-black" id="experience">
+                    <div className="font-extrabold text-5xl mb-10 ml-10">Experience</div>
+                    <hr />
                     <div>
-                        <h1 className ="font-bold text-xl ml-10">Student Developer</h1>
-                        <p className="font-bold italic ml-10">UBC BEST Design Team</p>
-                        <p>UBC BEST is a biomedical engineering student design team, where I am a part of the Multiple Sclerosis to Movement(M2M) sub-team. M2M&apos;s project is a data driven glove paired with a mobile app. The app has built in games to engage patients in rehabilitiation exercises, which then the sensors in the glove collect data on their hand movements to track the progress of each patient within the app.</p>
+                        <h1 className="font-bold text-xl ml-10 mt-5">Student Developer</h1>
+                        <p className="font-bold italic ml-10">UBC BEST</p>
+                        <div className="grid grid-cols-2">
+                            <ul className="list-disc list-inside ml-10">
+                                <li>UBC BEST is a biomedical engineering student design team, where I am a part of the Multiple Sclerosis to Movement(M2M) sub-team </li>
+                                <li>M2M&apos;s project is a data driven glove paired with a mobile app to enhance Multiple Sclerosis patients' rehabilitation process and track progress </li>
+                                <li>The app has built in games to engage patients in rehabilitiation exercises, and displays the data from the sensors to help track the progess</li>
+                                <li>I am helping</li>
+                            </ul>
+
+                        </div>
                     </div>
-                    
+
                 </div>
             </div>
         </div>
